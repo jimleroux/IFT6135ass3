6347317ed8f92adee421f39b5d5fb2f2756b5b73
Ishaan Kumar <ffishaan@gmail.com>
2019-04-16 13:04:28 -0400

    early stopping in vae

--- p3/train_vae.py
+++ p3/train_vae.py
@@ -15,19 +15,23 @@ def compute_loss(inputs, outputs, mu, logvar):
 def train_vae():
 
     batch_size = 64
-    epochs = 100
+    epochs = 1000
     latent_dimension = 100
+    patience = 10
 
     device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
     # load data
-    train_loader, valid_loader, test_loader = get_data_loader('data', batch_size)
+    train_loader, valid_loader, _ = get_data_loader('data', batch_size)
 
     model = VAE(latent_dimension).to(device)
     
     optim = Adam(model.parameters(), lr=1e-4)
 
+    val_greater_count = 0
+    last_val_loss = 0
     for e in range(epochs):
         running_loss = 0 
+        model.train()
         for i, (images, _) in enumerate(train_loader):
             images = images.to(device)
             model.zero_grad()
@@ -37,14 +41,29 @@ def train_vae():
             loss.backward()
             optim.step()
 
+        running_loss = running_loss/len(train_loader)
+        model.eval()
+        with torch.no_grad():
+            val_loss = 0
+            for images, _ in valid_loader:
+                outputs, mu, logvar = model(images)
+                loss = compute_loss(images, outputs, mu, logvar)
+                val_loss += loss
+            val_loss /= len(valid_loader)
+
+        if val_loss > last_val_loss:
+            val_greater_count +=1
+        else:
+            val_greater_count = 0
+        last_val_loss = val_loss
+
         torch.save({
             'epoch': e,
             'model': model.state_dict(),
             'running_loss': running_loss,
             'optim': optim.state_dict(),
         }, "vae_checkpoint_{}.pth".format(e))
-        print("Epoch: {} Loss: {}".format(e+1, running_loss.item()/len(train_loader)))
-
-
-
-
+        print("Epoch: {} Train Loss: {}".format(e+1, running_loss.item()))
+        print("Epoch: {} Val Loss: {}".format(e+1, val_loss.item()))
+        if val_greater_count >= patience:
+            break
