cfb16caf97a9e9fe81901d136a0992e013c4e9bf
Ishaan Kumar <ffishaan@gmail.com>
2019-04-15 23:09:03 -0400

    trainer corrections

--- p3/train_gan.py
+++ p3/train_gan.py
@@ -6,6 +6,7 @@ from torch.optim import Adam
 from gan_model import Generator, Discriminator
 from loader import get_data_loader
 
+
 def gradient_penalty(disc_model, real_images, fake_images):
     epsilon = real_images.new_empty(real_images.size())
     epsilon.uniform_()
@@ -13,7 +14,6 @@ def gradient_penalty(disc_model, real_images, fake_images):
     outputs = disc_model(intermediate)
     grads = torch.autograd.grad(outputs, intermediate, real_images.new_ones(), create_graph=True, retain_graph=True)
     return ((grads.norm(2, dim=1) - 1)**2).mean()
-
 
 
 def train_gan():
@@ -30,24 +30,21 @@ def train_gan():
     train_loader, valid_loader, test_loader = get_data_loader('data', batch_size)
 
     disc_model = Discriminator().to(device)
-    gen_model = Generator().to(device)
+    gen_model = Generator(latent_dimension).to(device)
     
     disc_optim = Adam(disc_model.parameters(), lr=1e-4)
     gen_optim = Adam(gen_model.parameters(), lr=1e-4)
 
-
-    normal_sampler = torch.distributions.MultivariateNormal(
-        torch.zeros(latent_dimension), torch.eye(latent_dimension)).to(device)
     for e in range(epochs):
         disc_loss = 0 
         gen_loss = 0
-        for i, images, _ in enumerate(train_loader):
+        for i, (images, _) in enumerate(train_loader):
             images = images.to(device)
             step = i+1
             if step % disc_update == 0:
                 disc_model.zero_grad()
                 # sample noise
-                noise = torch.randn((batch_size, latent_dimension))
+                noise = torch.randn((batch_size, latent_dimension)).to(device)
 
                 # loss on fake
                 inputs = gen_model(noise).detach()
@@ -58,25 +55,26 @@ def train_gan():
                 outputs = disc_model(images)
                 loss -= outputs.mean()
 
-                loss += lambduh*gradient_penalty(disc_model, images, inputs)
-                disc_loss += loss
+                # add gradient penalty
+                loss += lambduh * gradient_penalty(disc_model, images, inputs)
+
+                disc_loss += loss / batch_size
                 loss.backward()
                 disc_optim.step()
 
             if step % gen_update == 0:
                 gen_model.zero_grad()
 
-                noise = torch.randn((batch_size, latent_dimension))
+                noise = torch.randn((batch_size, latent_dimension)).to(device)
                 inputs = gen_model(noise)
                 with torch.no_grad():
                     outputs = disc_model(inputs)
                 loss = -outputs.mean()  
                 
-                gen_loss += loss
-
+                gen_loss += loss / batch_size
+                disc_model.zero_grad()
                 loss.backward()
                 gen_optim.step()
-
         print("Epoch: {} Disc loss: {}".format(e+1, disc_loss.item()))
         print("Epoch: {} Gen loss: {}".format(e+1, gen_loss.item()))
 
